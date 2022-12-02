c957ba1cecee3034d355639f766eac7b2fc699a1
Jesus Laiz <zheileman@users.noreply.github.com>
2022-12-02 10:33:28 +0000

    Add offence dates min items constraint
    At least 1 date must be declared for any given offence, to be considered valid.

--- lib/laa_crime_schemas/structs/offence.rb
+++ lib/laa_crime_schemas/structs/offence.rb
@@ -5,7 +5,7 @@ module LaaCrimeSchemas
     class Offence < Base
       attribute :name, Types::String
       attribute? :offence_class, Types::String.optional
-      attribute :dates, Types::Array.of(Types::JSON::Date)
+      attribute :dates, Types::Array.of(Types::JSON::Date).constrained(min_size: 1)
     end
   end
 end
