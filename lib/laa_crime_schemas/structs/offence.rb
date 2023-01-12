b4a58b5067a9f7267a0a08c7067e1b8f36554f0e
Jesus Laiz <zheileman@users.noreply.github.com>
2023-01-12 12:43:49 +0000

    Enforce new range format, both can't coexist
    It seems it is not possible to make both coexist.
    
    New submissions will use new format, but existing submitted applications will have to be migrated to the new format perhaps with an SQL migration.
    
    Another alternative is to wipe up the datastore and start fresh. If we do this, we can enforce some other attributes we made lenient in the past.

--- lib/laa_crime_schemas/structs/offence.rb
+++ lib/laa_crime_schemas/structs/offence.rb
@@ -5,15 +5,10 @@ module LaaCrimeSchemas
     class Offence < Base
       attribute :name, Types::String
       attribute? :offence_class, Types::String.optional
-      attribute :dates, Types::Array.constrained(min_size: 1)
-
-      # Use the following, more strict attribute definition once the
-      # existing applications conforms to the new format.
-      #
-      # attribute :dates, Types::Array.of(Base).constrained(min_size: 1) do
-      #   attribute :date_from, Types::JSON::Date
-      #   attribute :date_to, Types::JSON::Date.optional
-      # end
+      attribute :dates, Types::Array.of(Base).constrained(min_size: 1) do
+        attribute :date_from, Types::JSON::Date
+        attribute :date_to, Types::JSON::Date.optional
+      end
     end
   end
 end
