6a00c0b1003b15c142578b70503deeed708ec91b
Jesus Laiz <zheileman@users.noreply.github.com>
2023-05-31 16:46:10 +0100

    Add `passportable` bool attribute to offence object
    On the back of this PR on Apply, we now can propagate this from Apply to Datastore, even tho we don't need it just now.
    
    Marking attribute in the Struct as optional, because existing applications in the datastore will not have this.
    
    Not bumping schemas version yet, as there is no need to release a new version just for this.
    
    Will be bumped a bit later once we do some auditing of attributes, structs and schemas that is pending.

--- lib/laa_crime_schemas/structs/offence.rb
+++ lib/laa_crime_schemas/structs/offence.rb
@@ -4,7 +4,10 @@ module LaaCrimeSchemas
   module Structs
     class Offence < Base
       attribute :name, Types::String
+
       attribute? :offence_class, Types::String.optional
+      attribute? :passportable, Types::Bool
+
       attribute :dates, Types::Array.of(Base).constrained(min_size: 1) do
         attribute :date_from, Types::JSON::Date
         attribute :date_to, Types::JSON::Date.optional
