7d29c6170aa9a30a6e220a027a6c4fad2829a551
Jesus Laiz <zheileman@users.noreply.github.com>
2023-07-04 17:25:11 +0100

    CRIMAP-427 Refactor appeal attributes
    Removed no longer needed `appeal_with_changes_maat_id` attribute, because for both types of appeals, the attribute `appeal_maat_id` will be used.
    
    Added a new `appeal_lodged_date` attribute, of type date.
    
    The Struct is not enforcing presence of this new attribute, as existing applications in datastore lack it, but ideally should be made mandatory before going live.

--- lib/laa_crime_schemas/structs/case_details.rb
+++ lib/laa_crime_schemas/structs/case_details.rb
@@ -6,8 +6,10 @@ module LaaCrimeSchemas
       attribute :urn, Types::String.optional
       attribute :case_type, Types::CaseType
       attribute? :offence_class, Types::OffenceClass.optional
+
+      # TODO: mark `appeal_lodged_date` attr as mandatory (keep optional value)
       attribute :appeal_maat_id, Types::String.optional
-      attribute :appeal_with_changes_maat_id, Types::String.optional
+      attribute? :appeal_lodged_date, Types::JSON::Date.optional
       attribute :appeal_with_changes_details, Types::String.optional
 
       attribute :offences, Types::Array.of(Offence).constrained(min_size: 1)
