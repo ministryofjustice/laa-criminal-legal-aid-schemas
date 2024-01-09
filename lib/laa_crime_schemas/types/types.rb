912ba25ec6153f0725813cb9381f0dee4ce4c7f1
timpeat <tim@timpeat.com>
2024-01-09 17:22:33 +0000

    CRIMAPP-290 add PSE to application type (#74)
    * CRIMAPP-290 add PSE to application type
    
    * Add change in financial circumstances application_type
    
    * Add PSE and CIFC application_type

--- lib/laa_crime_schemas/types/types.rb
+++ lib/laa_crime_schemas/types/types.rb
@@ -17,6 +17,8 @@ module LaaCrimeSchemas
 
     APPLICATION_TYPES = %w[
       initial
+      post_submission_evidence
+      change_in_financial_circumstances
     ].freeze
     ApplicationType = String.enum(*APPLICATION_TYPES)
 
