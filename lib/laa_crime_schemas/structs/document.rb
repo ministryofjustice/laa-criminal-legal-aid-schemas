966aaebc2e1a78757a32cc02a1fa8066e1d9c334
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-01-10 09:15:09 +0000

    Add pse to schema and application type to document struct

--- lib/laa_crime_schemas/structs/document.rb
+++ lib/laa_crime_schemas/structs/document.rb
@@ -7,6 +7,7 @@ module LaaCrimeSchemas
       attribute :filename, Types::String
       attribute :content_type, Types::String
       attribute :file_size, Types::Coercible::Integer
+      attribute? :application_type, Types::String
 
       # Virus scanning
       attribute :scan_status, Types::VirusScanStatus
