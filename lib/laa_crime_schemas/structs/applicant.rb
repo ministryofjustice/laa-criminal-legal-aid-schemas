507fd3c97b27d9db0a99ee4a4a93dff8ce17e238
timpeat <tim@timpeat.com>
2024-01-25 13:04:28 +0000

    Do not require correspondence_address type

--- lib/laa_crime_schemas/structs/applicant.rb
+++ lib/laa_crime_schemas/structs/applicant.rb
@@ -5,11 +5,11 @@ module LaaCrimeSchemas
     class Applicant < Person
       attribute :date_of_birth, Types::JSON::Date
       attribute? :benefit_type, Types::BenefitType.optional
-      attribute :nino, Types::String.optional
-      attribute :home_address, Address.optional
-      attribute :correspondence_address, Address.optional
-      attribute :correspondence_address_type, Types::CorrespondenceAddressType
-      attribute :telephone_number, Types::String.optional
+      attribute? :nino, Types::String.optional
+      attribute? :home_address, Address.optional
+      attribute? :correspondence_address, Address.optional
+      attribute? :correspondence_address_type, Types::CorrespondenceAddressType.optional
+      attribute? :telephone_number, Types::String.optional
     end
   end
 end
