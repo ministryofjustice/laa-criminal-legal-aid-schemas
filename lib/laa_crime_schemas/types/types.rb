6441cf7597f411eb6e6e5cf33144390184c011f1
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-01-29 17:02:57 +0000

    Remove employment status (no longer required)

--- lib/laa_crime_schemas/types/types.rb
+++ lib/laa_crime_schemas/types/types.rb
@@ -179,8 +179,6 @@ module LaaCrimeSchemas
                                 policies
                               ])
 
-    EmploymentStatusType = String.enum(*%w[employed self-employed none])
-
     EmploymentType = String.enum(*%w[employed self-employed business_partnership director shareholder not_working])
 
     DependantAge = Integer.constrained(lt: 18)
