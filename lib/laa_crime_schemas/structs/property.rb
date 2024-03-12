309847a4f62d9a017f75c5e30cbcab692df23bdd
Ripan <er.ripankumar@gmail.com>
2024-03-12 12:00:10 +0000

    add  schema support for property and property address

--- lib/laa_crime_schemas/structs/property.rb
+++ lib/laa_crime_schemas/structs/property.rb
@@ -4,18 +4,18 @@ module LaaCrimeSchemas
   module Structs
     class Property < Base
       attribute :property_type, Types::PropertyType
-      attribute :house_type, Types::HouseType
-      attribute :custom_house_type, Types::String
+      attribute :house_type, Types::HouseType.optional
+      attribute :custom_house_type, Types::String.optional
       attribute :size_in_acres, Types::Integer.optional
       attribute :usage, Types::String.optional
-      attribute :bedrooms, Types::Integer
+      attribute :bedrooms, Types::Integer.optional
       attribute :value, Types::PenceSterling
       attribute :outstanding_mortgage, Types::PenceSterling
       attribute :percentage_applicant_owned, Types::Integer
       attribute :percentage_partner_owned, Types::Integer.optional
       attribute :is_home_address, Types::YesNoValue
       attribute :has_other_owners, Types::YesNoValue
-      # attribute :address, Types::JSON.optional # TODO
+      attribute :address, Address.optional
     end
   end
 end
---
+++ spec/laa_crime_schemas/structs/property_spec.rb
@@ -0,0 +1,41 @@
+RSpec.describe LaaCrimeSchemas::Structs::Property do
+  subject { described_class.new(attributes) }
+
+  let(:valid_fixture) { 'application/1.0/means.json' }
+
+  let(:attributes) do
+    JSON.parse(file_fixture(valid_fixture).read).fetch('capital_details').fetch('properties')[0]
+  end
+
+  describe '.new' do
+    context 'for a valid property object' do
+      it 'builds a property struct' do
+        expect(subject.property_type).to eq('residential')
+        expect(subject.house_type).to eq('custom')
+        expect(subject.custom_house_type).to eq('custom house type')
+        expect(subject.size_in_acres).to eq(100)
+        expect(subject.usage).to eq("usage details")
+        expect(subject.bedrooms).to eq(92)
+        expect(subject.value).to eq(7900)
+        expect(subject.outstanding_mortgage).to eq(1100)
+        expect(subject.percentage_applicant_owned).to eq(80)
+        expect(subject.percentage_partner_owned).to eq(20)
+        expect(subject.is_home_address).to eq('yes')
+        expect(subject.has_other_owners).to eq('no')
+        expect(subject.address.address_line_one).to eq('address_line_one_x')
+        expect(subject.address.address_line_two).to eq('address_line_two_x')
+        expect(subject.address.city).to eq('city_x')
+        expect(subject.address.country).to eq('country_x')
+        expect(subject.address.postcode).to eq('postcode_x')
+      end
+    end
+
+    context 'for an invalid property object' do
+      let(:attributes) { super().merge('property_type' => 'invalid') }
+
+      it 'raises an error' do
+        expect { subject }.to raise_error(Dry::Struct::Error, /property_type/)
+      end
+    end
+  end
+end
