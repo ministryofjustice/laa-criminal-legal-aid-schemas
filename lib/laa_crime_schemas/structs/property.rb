3c0888d290782deb12e387f2dc85392c6273c297
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-03-13 15:34:04 +0000

    Change percentage types to float

--- lib/laa_crime_schemas/structs/property.rb
+++ lib/laa_crime_schemas/structs/property.rb
@@ -11,8 +11,8 @@ module LaaCrimeSchemas
       attribute :bedrooms, Types::Integer.optional
       attribute :value, Types::PenceSterling
       attribute :outstanding_mortgage, Types::PenceSterling
-      attribute :percentage_applicant_owned, Types::Integer
-      attribute :percentage_partner_owned, Types::Integer.optional
+      attribute :percentage_applicant_owned, Types::Float
+      attribute :percentage_partner_owned, Types::Float.optional
       attribute :is_home_address, Types::YesNoValue.optional
       attribute :has_other_owners, Types::YesNoValue
       attribute :address, Address.optional
