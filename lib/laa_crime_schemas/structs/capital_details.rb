50a4ad93646ccfa9b7d06340ae9795645667e562
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-03-13 15:18:15 +0000

    Make capital details fields optional

--- lib/laa_crime_schemas/structs/capital_details.rb
+++ lib/laa_crime_schemas/structs/capital_details.rb
@@ -3,13 +3,13 @@
 module LaaCrimeSchemas
   module Structs
     class CapitalDetails < Base
-      attribute :has_premium_bonds, Types::YesNoValue
-      attribute :premium_bonds_total_value, Types::PenceSterling.optional
-      attribute :premium_bonds_holder_number, Types::String.optional
+      attribute? :has_premium_bonds, Types::YesNoValue
+      attribute? :premium_bonds_total_value, Types::PenceSterling.optional
+      attribute? :premium_bonds_holder_number, Types::String.optional
 
-      attribute :will_benefit_from_trust_fund, Types::YesNoValue
-      attribute :trust_fund_amount_held, Types::PenceSterling.optional
-      attribute :yearly_dividend, Types::PenceSterling.optional
+      attribute? :will_benefit_from_trust_fund, Types::YesNoValue
+      attribute? :trust_fund_amount_held, Types::PenceSterling.optional
+      attribute? :yearly_dividend, Types::PenceSterling.optional
 
       attribute? :savings, Types::Array.of(Saving).default([].freeze)
       attribute? :properties, Types::Array.of(Property).default([].freeze)
