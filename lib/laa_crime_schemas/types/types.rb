53ab3c1d35acdf2038d9f9971f2bb6c9a73ffd4e
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-03-21 13:13:23 +0000

    Create shared income payment type constant to use across apps

--- lib/laa_crime_schemas/types/types.rb
+++ lib/laa_crime_schemas/types/types.rb
@@ -66,18 +66,20 @@ module LaaCrimeSchemas
     ].freeze
     FirstHearingAnswerValues = String.enum(*FIRST_HEARING_ANSWER_VALUES)
 
-    IncomePaymentType = String.enum(*%w[
-                                      private_pension
-                                      state_pension
-                                      maintenance
-                                      interest_investment
-                                      student_loan_grant
-                                      board_from_family
-                                      rent
-                                      financial_support_with_access
-                                      from_friends_relatives
-                                      other
-                                    ])
+    # NOTE: maintain order as per designs when editing this list
+    INCOME_PAYMENT_TYPES = %w[
+                             maintenance
+                             private_pension
+                             state_pension
+                             interest_investment
+                             student_loan_grant
+                             board_from_family
+                             rent
+                             financial_support_with_access
+                             from_friends_relatives
+                             other
+                           ].freeze
+    IncomePaymentType = String.enum(*INCOME_PAYMENT_TYPES)
 
     IncomeBenefitType = String.enum(*%w[
                                       child
