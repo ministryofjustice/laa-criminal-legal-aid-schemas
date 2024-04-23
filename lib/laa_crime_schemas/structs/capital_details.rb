48107816d1c8316f9817b22404c966b333ca11fd
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-04-23 13:24:06 +0100

    [CRIMAPP-735] Add additional means attributes (#132)
    * Add additional means attributes
    
    * Amend attribute name

--- lib/laa_crime_schemas/structs/capital_details.rb
+++ lib/laa_crime_schemas/structs/capital_details.rb
@@ -11,9 +11,16 @@ module LaaCrimeSchemas
       attribute? :trust_fund_amount_held, Types::PenceSterling.optional
       attribute? :trust_fund_yearly_dividend, Types::PenceSterling.optional
 
+      attribute? :has_no_savings, Types::YesNoValue.optional
       attribute? :savings, Types::Array.of(Saving).default([].freeze)
+
+      attribute? :has_no_properties, Types::YesNoValue.optional
       attribute? :properties, Types::Array.of(Property).default([].freeze)
+
+      attribute? :has_no_investments, Types::YesNoValue.optional
       attribute? :investments, Types::Array.of(Investment).default([].freeze)
+
+      attribute? :has_national_savings_certificates, Types::YesNoValue.optional
       attribute? :national_savings_certificates, Types::Array.of(NationalSavingsCertificate).default([].freeze)
 
       attribute? :has_frozen_income_or_assets, Types::YesNoValue.optional
