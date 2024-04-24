afb7418220e0e825aa796e9a3c797fb431da621e
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2024-04-24 19:18:43 +0100

    CRIMAPP-649 Allow empty evidence_details for. legacy applications

--- lib/laa_crime_schemas/structs/evidence_details.rb
+++ lib/laa_crime_schemas/structs/evidence_details.rb
@@ -3,8 +3,8 @@
 module LaaCrimeSchemas
   module Structs
     class EvidenceDetails < Base
-      attribute :last_run_at, Types::JSON::DateTime
-      attribute :evidence_prompts, Types::Array.of(Prompt).default([].freeze)
+      attribute? :last_run_at, Types::JSON::DateTime.optional
+      attribute? :evidence_prompts, Types::Array.of(Prompt).default([].freeze)
     end
   end
 end
--- spec/laa_crime_schemas/structs/evidence_details_spec.rb
+++ spec/laa_crime_schemas/structs/evidence_details_spec.rb
@@ -1,12 +1,12 @@
 RSpec.describe LaaCrimeSchemas::Structs::EvidenceDetails do
   subject { described_class.new(attributes) }
 
-  let(:attributes) do
-    LaaCrimeSchemas.fixture(1.0) { |json| json['evidence_details'] }
-  end
-
   describe '.new' do
     context 'for a valid evidence details object' do
+      let(:attributes) do
+        LaaCrimeSchemas.fixture(1.0) { |json| json['evidence_details'] }
+      end
+
       it 'builds the evidence details struct' do
         expect(subject.last_run_at.strftime('%F %T')).to eq(
           DateTime.new(2024, 4, 24, 13, 28, 30).strftime('%F %T')
@@ -19,6 +19,17 @@ RSpec.describe LaaCrimeSchemas::Structs::EvidenceDetails do
         expect(subject.evidence_prompts.first.key).to eq 'national_insurance_32'
         expect(subject.evidence_prompts.first.run.other.result).to eq true
         expect(subject.evidence_prompts.first.run.other.prompt.first).to eq 'their National Insurance number'
+      end
+    end
+
+    context 'for a nil evidence details object' do
+      let(:attributes) do
+        nil
+      end
+
+      it 'builds the evidence details struct' do
+        expect(subject.last_run_at).to be nil
+        expect(subject.evidence_prompts).to eq []
       end
     end
   end
