9a0f2cfd92360ba85be58a03dc0983db1bc7b048
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-05-20 14:59:54 +0100

    WIP add passporting benefit attributes

--- lib/laa_crime_schemas/structs/applicant.rb
+++ lib/laa_crime_schemas/structs/applicant.rb
@@ -6,6 +6,7 @@ module LaaCrimeSchemas
       attribute :date_of_birth, Types::JSON::Date
       attribute? :benefit_type, Types::BenefitType.optional
       attribute? :last_jsa_appointment_date, Types::JSON::Date.optional
+      attribute? :has_nino, Types::String.optional
       attribute? :nino, Types::String.optional
       attribute? :home_address, Address.optional
       attribute? :correspondence_address, Address.optional
@@ -13,6 +14,12 @@ module LaaCrimeSchemas
       attribute? :telephone_number, Types::String.optional
       attribute? :residence_type, Types::ResidenceType.optional
       attribute? :relationship_to_owner_of_usual_home_address, Types::String.optional
+
+      attribute? :passporting_benefit, Types::Bool.optional
+      attribute? :has_benefit_evidence, Types::String.optional
+      attribute? :will_enter_nino, Types::String.optional
+      attribute? :confirm_details, Types::String.optional
+      attribute? :confirm_dwp_result, Types::String.optional
 
       attribute? :has_partner, Types::String.optional
       attribute? :relationship_to_partner, Types::RelationshipType.optional
