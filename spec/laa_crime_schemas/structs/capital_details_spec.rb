e60b050dcafa375c649d38f60b946de23a2683ff
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2024-06-03 10:49:18 +0100

    CRIMAPP-973 Enforce yes/no in JSON Schema

--- spec/laa_crime_schemas/structs/capital_details_spec.rb
+++ spec/laa_crime_schemas/structs/capital_details_spec.rb
@@ -96,15 +96,14 @@ RSpec.describe LaaCrimeSchemas::Structs::CapitalDetails do
       let(:key) { 'will_benefit_from_trust_fund' }
       subject(:value) { struct.send(key) }
 
-      # :nocov:
-      context 'when nil', skip: 'CRIMAPP-973 Apply submitting null' do
+      # NOTE: Corresponding JSON Schema field does not allow `null`
+      context 'when nil' do
         let(:attributes) { super().merge(key => nil) }
 
-        it 'raises an error' do
-          expect { subject }.to raise_error(Dry::Struct::Error, /#{key}/)
+        it 'does not raise an error' do
+          expect { subject }.not_to raise_error(Dry::Struct::Error, /#{key}/)
         end
       end
-      # :nocov:
 
       context 'when not a yes/no' do
         let(:attributes) { super().merge('will_benefit_from_trust_fund' => 'NotYesOrNo') }
