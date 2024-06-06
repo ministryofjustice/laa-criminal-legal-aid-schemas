4f198e136fdaccd4ffb198f94b5ebd7514498d77
Ripan <ripan.kumar@digital.justice.gov.uk>
2024-06-06 13:12:27 +0100

    CRIMAPP-875 - Refactor Income and Outgoings payments (#157)
    * Refactor Income and Outgoings payments
    
    * Update schema version

--- spec/laa_crime_schemas/types/types_spec.rb
+++ spec/laa_crime_schemas/types/types_spec.rb
@@ -11,5 +11,107 @@ RSpec.describe LaaCrimeSchemas::Types do
         expect(LaaCrimeSchemas::Types::OwnershipType[]).to eq('applicant')
       end
     end
+
+    context "income payments" do
+      context 'for OtherIncomePaymentType' do
+        it 'returns other income payment types' do
+          expect(LaaCrimeSchemas::Types::OtherIncomePaymentType.values).to match_array(%w[
+          maintenance
+          private_pension
+          state_pension
+          interest_investment
+          student_loan_grant
+          board_from_family
+          rent
+          financial_support_with_access
+          from_friends_relatives
+          other
+        ])
+        end
+      end
+
+      context 'for EmploymentIncomePaymentType' do
+        it 'returns employment income payment types' do
+          expect(LaaCrimeSchemas::Types::EmploymentIncomePaymentType.values).to match_array(%w[
+          employment
+          work_benefits
+        ])
+        end
+      end
+
+      context 'for IncomePaymentType' do
+        it 'returns all income payment types' do
+          expect(LaaCrimeSchemas::Types::IncomePaymentType.values).to match_array(%w[
+            maintenance
+            private_pension
+            state_pension
+            interest_investment
+            student_loan_grant
+            board_from_family
+            rent
+            financial_support_with_access
+            from_friends_relatives
+            other
+            employment
+            work_benefits
+          ])
+        end
+      end
+    end
+
+    context "outgoings payments" do
+      context 'for OtherOutgoingsPaymentType' do
+        it 'returns all income payment types' do
+          expect(LaaCrimeSchemas::Types::OtherOutgoingsPaymentType.values).to match_array(%w[
+            rent
+            mortgage
+            board_and_lodging
+            council_tax
+            childcare
+            maintenance
+            legal_aid_contribution
+        ])
+        end
+
+        context 'for EmploymentOutgoingsPaymentType' do
+          it 'returns all income payment types' do
+            expect(LaaCrimeSchemas::Types::EmploymentOutgoingsPaymentType.values).to match_array(%w[
+              self_assessment_tax_bill
+          ])
+          end
+        end
+
+        context 'for OutgoingsType' do
+          it 'returns all income payment types' do
+            expect(LaaCrimeSchemas::Types::OutgoingsType.values).to match_array(%w[
+              rent
+              mortgage
+              board_and_lodging
+              council_tax
+              childcare
+              maintenance
+              legal_aid_contribution
+              self_assessment_tax_bill
+          ])
+          end
+        end
+      end
+
+    end
+
+    context "income benefits" do
+      context 'for IncomeBenefitType' do
+        it 'returns all income payment types' do
+          expect(LaaCrimeSchemas::Types::IncomeBenefitType.values).to match_array(%w[
+            child
+            working_or_child_tax_credit
+            incapacity
+            industrial_injuries_disablement
+            jsa
+            other
+          ])
+        end
+      end
+    end
   end
 end
