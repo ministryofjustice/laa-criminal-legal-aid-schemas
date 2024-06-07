256f2a7fb64d3e1bbd6d056f95e226006373dbae
Hibo <51047911+hiboabd@users.noreply.github.com>
2024-06-07 09:43:06 +0100

    [CRIMAPP-890] Add benefit check attributes for partner (#155)
    * Add benefit check attributes for partner
    
    * Increment schema version
    
    * Increment schema version

--- lib/laa_crime_schemas/structs/partner.rb
+++ lib/laa_crime_schemas/structs/partner.rb
@@ -15,6 +15,13 @@ module LaaCrimeSchemas
       attribute? :nino, Types::String.optional
       attribute? :home_address, Address.optional
 
+      attribute? :benefit_check_result, Types::Bool.optional
+      attribute? :benefit_check_status, Types::BenefitCheckStatus.optional
+      attribute? :has_benefit_evidence, Types::String.optional
+      attribute? :will_enter_nino, Types::String.optional
+      attribute? :confirm_details, Types::String.optional
+      attribute? :confirm_dwp_result, Types::String.optional
+
       attribute? :involvement_in_case, Types::CaseInvolvement.optional
       attribute? :conflict_of_interest, Types::YesNoValue.optional
       attribute? :has_same_address_as_client, Types::YesNoValue.optional
