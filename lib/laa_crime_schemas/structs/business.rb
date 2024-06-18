31e80c7662dfa2b32c6c8947dc7d5d9b72a2449b
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-06-18 13:20:48 +0100

    Amend business struct

--- lib/laa_crime_schemas/structs/business.rb
+++ lib/laa_crime_schemas/structs/business.rb
@@ -4,23 +4,22 @@ module LaaCrimeSchemas
   module Structs
     class Business < Base
       attribute :ownership_type, Types::OwnershipType
-      attribute :type, Types::BusinessType
-      attribute :trading_name, Types::String
-      attribute :address, Address
-      attribute :description, Types::String
-      attribute :trading_date, Types::JSON::Date
-      attribute :has_additional_owners, Types::YesNoValue
-      attribute? :owners, Types::String.optional
-      attribute :has_employees, Types::YesNoValue
+      attribute :business_type, Types::BusinessType
+      attribute? :trading_name, Types::String
+      attribute? :address, Address
+      attribute? :description, Types::String
+      attribute? :trading_start_date, Types::JSON::Date
+      attribute? :has_additional_owners, Types::YesNoValue
+      attribute? :additional_owners, Types::String.optional
+      attribute? :has_employees, Types::YesNoValue
       attribute? :number_of_employees, Types::Integer.optional
       attribute? :salary, Types::PenceSterling.optional
-      attribute? :total_income, Types::PenceSterling.optional
-      attribute? :profit_share, Types::Float.optional
+      attribute? :total_income_share_sales, Types::PenceSterling.optional
+      attribute? :percentage_profit_share, Types::Float.optional
 
-      attribute? :financials, Types::Coercible::Array.of(Base).default([].freeze) do
-        attribute :financial_type, Types::FinancialType
-        attributes_from Amount
-      end
+      attribute? :turnover, Amount
+      attribute? :drawings, Amount
+      attribute? :profit, Amount
     end
   end
 end
--- lib/laa_crime_schemas/types/types.rb
+++ lib/laa_crime_schemas/types/types.rb
@@ -289,7 +289,6 @@ module LaaCrimeSchemas
                                          confirmed])
 
     BusinessType = String.enum(*%w[self_employed partnership director_or_shareholder])
-    FinancialType = String.enum(*%w[turnover drawings profit])
   end
 end
 # rubocop:enable Metrics/ModuleLength
