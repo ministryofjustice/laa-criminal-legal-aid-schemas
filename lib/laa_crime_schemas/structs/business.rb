2f4f3d538f8d6d14ec390eead1a1f2772b7e10ca
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-06-18 13:26:56 +0100

    Fix specs

--- lib/laa_crime_schemas/structs/business.rb
+++ lib/laa_crime_schemas/structs/business.rb
@@ -5,21 +5,21 @@ module LaaCrimeSchemas
     class Business < Base
       attribute :ownership_type, Types::OwnershipType
       attribute :business_type, Types::BusinessType
-      attribute? :trading_name, Types::String
-      attribute? :address, Address
-      attribute? :description, Types::String
-      attribute? :trading_start_date, Types::JSON::Date
-      attribute? :has_additional_owners, Types::YesNoValue
+      attribute? :trading_name, Types::String.optional
+      attribute? :address, Address.optional
+      attribute? :description, Types::String.optional
+      attribute? :trading_start_date, Types::JSON::Date.optional
+      attribute? :has_additional_owners, Types::YesNoValue.optional
       attribute? :additional_owners, Types::String.optional
-      attribute? :has_employees, Types::YesNoValue
+      attribute? :has_employees, Types::YesNoValue.optional
       attribute? :number_of_employees, Types::Integer.optional
       attribute? :salary, Types::PenceSterling.optional
       attribute? :total_income_share_sales, Types::PenceSterling.optional
       attribute? :percentage_profit_share, Types::Float.optional
 
-      attribute? :turnover, Amount
-      attribute? :drawings, Amount
-      attribute? :profit, Amount
+      attribute? :turnover, Amount.optional
+      attribute? :drawings, Amount.optional
+      attribute? :profit, Amount.optional
     end
   end
 end
