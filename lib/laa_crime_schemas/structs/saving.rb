a59e1cb00a1a6659f4226b792ee35aaac4c76ce5
timpeat <tim@timpeat.com>
2024-07-24 11:03:32 +0100

    Saving supports draft

--- lib/laa_crime_schemas/structs/saving.rb
+++ lib/laa_crime_schemas/structs/saving.rb
@@ -4,14 +4,14 @@ module LaaCrimeSchemas
   module Structs
     class Saving < Base
       attribute :saving_type, Types::SavingType
-      attribute :provider_name, Types::String
+      attribute? :provider_name, Types::String.optional
       attribute :account_balance, Types::PenceSterling
-      attribute :sort_code, Types::String
-      attribute :account_number, Types::String
-      attribute :is_overdrawn, Types::YesNoValue
-      attribute :are_wages_paid_into_account, Types::YesNoValue
+      attribute? :sort_code, Types::String.optional
+      attribute? :account_number, Types::String.optional
+      attribute? :is_overdrawn, Types::YesNoValue.optional
+      attribute? :are_wages_paid_into_account, Types::YesNoValue.optional
       attribute? :are_partners_wages_paid_into_account, Types::YesNoValue.optional
-      attribute :ownership_type, Types::OwnershipType
+      attribute? :ownership_type, Types::OwnershipType.optional
     end
   end
 end
